0344a322457dc039241aeeb8248814be16797f5d
bonan <zbn.dale@gmail.com>
2022-12-29 17:28:45 +0000

    Make it 8 by 8

--- experiment/js/helper.js
+++ experiment/js/helper.js
@@ -1,21 +1,26 @@
 
-// // Generate html table cells
-// let n=10
-// let tabCodeList = []
-// for (let i = 0; i < n; i++) {
-//   let tcCodeList = []
-//   for (let j = 0; j < n; j++) {
-//     let cId = i.toString() + j.toString()
-//     let tcCdoe = `<td id="task-grid-tab-c${cId}" onclick=myFunc(this)></td>`
-//     tcCodeList.push(tcCdoe)
-//   }
-//   let trCode = '<tr> ' + tcCodeList.join() + ' </tr>'
-//   tabCodeList.push(trCode)
-// }
-// console.log(tabCodeList.join('\n'))
+let n=8
+
+// Generate html table cells
+let tabCodeList = []
+for (let i = 0; i < n; i++) {
+  let tcCodeList = []
+  for (let j = 0; j < n; j++) {
+    let cId = i.toString() + j.toString()
+    let tcCdoe = `<td id="task-grid-tab-c${cId}" onclick=myFunc(this)></td>`
+    tcCodeList.push(tcCdoe)
+  }
+  let trCode = '<tr> ' + tcCodeList.join('') + ' </tr>'
+  tabCodeList.push(trCode)
+}
+console.log(tabCodeList.join('\n'))
+
+console.log('\n')
+console.log('\n')
+console.log('\n')
+
 
 // Generate global vars
-let n=10
 let varList = []
 for (let i = 0; i < n; i++) {
   for (let j = 0; j < n; j++) {
